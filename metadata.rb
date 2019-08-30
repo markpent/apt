77b677ae25439d989db37bce91d4d991d9f174b2
Mark Pentland <mpentland@deconetwork.com>
2019-08-30 15:04:44 +1000

    bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@chef.io'
 license 'Apache-2.0'
 description 'Configures apt and apt caching.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '7.2.1'
+version '7.2.2'
 
 %w(ubuntu debian).each do |os|
   supports os
