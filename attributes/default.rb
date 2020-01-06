1ed51d4ba3046a088b9a502518c43dda6226dda7
Mark Pentland <mpentland@deconetwork.com>
2020-01-06 15:03:22 +1000

    add ability to specify proxy for apt-cacher-ng and a separate ssl proxy for apt

--- attributes/default.rb
+++ attributes/default.rb
@@ -20,6 +20,7 @@
 default['apt']['cacher_dir'] = '/var/cache/apt-cacher-ng'
 default['apt']['cacher_interface'] = nil
 default['apt']['cacher_port'] = 3142
+default['apt']['cacher_proxy'] = nil
 default['apt']['compiletime'] = false
 default['apt']['compile_time_update'] = false
 default['apt']['key_proxy'] = ''
